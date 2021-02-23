f93580374776ba31ae049fe33f9f701007aa48d5
Michał <michal@winami.com>
2021-02-23 18:19:31 +0100

    virgin environment

---
+++ src/main/java/pl/javastart/cook_app/CookAppApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.cook_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/cook_app/CookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.cook_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
