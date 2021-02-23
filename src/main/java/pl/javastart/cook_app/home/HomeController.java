9a4c1f64200ad330058bed21bc74ab2bddc8ee2e
Michał <michal@winami.com>
2021-02-24 00:54:05 +0100

    Step 1	frontend: home, top10, alphabetical, categories - working

---
+++ src/main/java/pl/javastart/cook_app/home/HomeController.java
@@ -0,0 +1,18 @@
+package pl.javastart.cook_app.home;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    public HomeController() {
+    }
+
+    @GetMapping("/")
+    public String homePage(){
+
+        return "home";
+    }
+}
